40a8d6fbe0e2a91b85b0420db8eb989dd16c0fca
Maria S <maria.sumedre@gmail.com>
2019-08-23 16:12:15 +0300

    item-fetch: fetch item in component pass down to upvote

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <ItemComponent />
+        <ItemComponent itemId={20775773}/>
     </div>
   );
 }
--- src/utils/ApiEndpoints.js
+++ src/utils/ApiEndpoints.js
@@ -1,18 +1,18 @@
 const BASE_URL = "https://hacker-news.firebaseio.com";
 
 class ApiEndpoints {
-    static getNewsItem(itemId) {
+    static getNewsItem(itemId, callback) {
         let request = new Request(`${BASE_URL}/v0/item/${itemId}.json`);
         fetch(request)
             .then(response => {
                 if (response.status === 200) {
-                    return response.json();
+                    return response.json();;
                 } else {
                     throw new Error('Something went wrong on api server!');
                 }
             })
             .then(response => {
-                console.debug(response);
+                callback && callback(response);
             }).catch(error => {
                 console.error(error);
             });
