bfd285570bf737b0664599d3286c79c23626d4fb
Maria S <maria.sumedre@gmail.com>
2019-08-23 13:54:36 +0300

    Clean app, add simple item component

---
+++ src/components/item/ItemComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ItemComponent extends React.Component {
+    render() {
+        return (
+            <p> Item Component </p>
+        )
+    }
+}
+
+export default ItemComponent;
